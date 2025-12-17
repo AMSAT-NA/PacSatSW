0d85f1fcefec523288bd6c63fd0519238d502082
Corey Minyard <corey@minyard.net>
2025-12-17 07:56:59 -0600

    Set GPIOA 2 to a pull up
    We want it high if nothing is driving it.
    
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- hcg/source/gio.c
+++ hcg/source/gio.c
@@ -103,7 +103,7 @@ void gioInit(void)
     /** - Port A pullup / pulldown selection */
     gioPORTA->PSL  = (uint32)((uint32)0U << 0U)  /* Bit 0 */
                    | (uint32)((uint32)0U << 1U)  /* Bit 1 */
-                   | (uint32)((uint32)0U << 2U)  /* Bit 2 */
+                   | (uint32)((uint32)1U << 2U)  /* Bit 2 */
                    | (uint32)((uint32)0U << 3U)  /* Bit 3 */
                    | (uint32)((uint32)0U << 4U)  /* Bit 4 */
                    | (uint32)((uint32)0U << 5U)  /* Bit 5 */
